07c67f4898f579871d8adb9636eff9f04155af3a
quietdw <1044569102@qq.com>
2019-04-23 00:04:45 +0800

    重写login页面，使用wx.login后把登陆数据传给接口

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,54 +1,45 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const { app_id, app_secret } = app.globalData
+
+import request from "../../utils/request.js"
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad() {
+
+  },
+  onShow(){
+    request.http.get('/todos').then(res=>{
+       console.log(res)
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+  bindGetUserInfo(e) {
+    let iv = e.detail.iv
+    let encrypted_data = e.detail.encryptedData
+    let code
+
+    wx.login({
+      success(res){
+        code = res.code
+        request.http.post('/sign_in/mini_program_user', {
+          code,
+          iv,
+          encrypted_data,
+          app_id,
+          app_secret
+        }).then(res => {
+          wx.setStorageSync('me', res.data.resource)
+          wx.setStorageSync('X-token', res.header['X-token'])
+          console.log(1)
+          wx.switchTab({
+            url: '/pages/home/home'
           })
-        }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+        }) 
+      }
     })
+    
   }
 })
