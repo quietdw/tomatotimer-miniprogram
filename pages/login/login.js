7092b5f0f742b78701b1990cfa0fe90706e42b82
quietdw <1044569102@qq.com>
2019-04-23 12:47:00 +0800

    删除部分测试代码

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,37 +1,32 @@
 //index.js
 //获取应用实例
 const app = getApp()
-const { app_id, app_secret } = app.globalData
+const {
+  app_id,
+  app_secret
+} = app.globalData
 
 import request from "../../utils/request.js"
 
 Page({
-  data: {
-  },
+  data: {},
   onLoad() {
 
   },
-  onShow(){
-    request.http.get('/todos').then(res=>{
-       console.log(res)
-    })
-  },
+  onShow() {},
   bindGetUserInfo(e) {
     let iv = e.detail.iv
     let encrypted_data = e.detail.encryptedData
-
     this.wxLogin(iv, encrypted_data)
-    
-  },
-  wxLogin(iv, encrypted_data){
+  }, 
+  wxLogin(iv, encrypted_data) {
     wx.login({
-      success:(res)=> {
-        console.log(res)
+      success: (res) => {
         this.meLogin(iv, encrypted_data, res.code)
       }
     })
   },
-  meLogin(iv, encrypted_data, code){
+  meLogin(iv, encrypted_data, code) {
     request.http.post('/sign_in/mini_program_user', {
       code,
       iv,
@@ -39,13 +34,13 @@ Page({
       app_id,
       app_secret
     }).then(res => {
-     this.saveData(res)
+      this.saveData(res)
       wx.switchTab({
         url: '/pages/home/home'
       })
     })
   },
-  saveData(res){
+  saveData(res) {
     wx.setStorageSync('me', res.data.resource)
     wx.setStorageSync('X-token', res.header['X-token'])
   }
